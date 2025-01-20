ff29e81dad3f5cf02b4319afcbec0a37499f3386
Rift <hegide.dev@gmail.com>
2025-01-19 22:10:18 -0500

    card effect

---
+++ Game.js
@@ -0,0 +1,55 @@
+const rock = document.querySelector("#Rock");
+const paper = document.querySelector("#Paper");
+const scissors = document.querySelector("#Scissors");
+
+rock.addEventListener("mouseover", () => {
+  const img = rock.querySelector("img");
+  const text = rock.querySelector("p");
+
+  img.src = "elements/hand-back-fist-solid.svg";
+  rock.style.backgroundColor = "black";
+  text.style.color = "white";
+});
+rock.addEventListener("mouseout", () => {
+  const img = rock.querySelector("img");
+  const text = rock.querySelector("p");
+
+  img.src = "elements/hand-back-fist-regular.svg";
+  rock.style.backgroundColor = "white";
+  text.style.color = "black";
+});
+
+paper.addEventListener("mouseover", () => {
+  const img = paper.querySelector("img");
+  const text = paper.querySelector("p");
+
+  img.src = "elements/hand-solid.svg";
+  paper.style.backgroundColor = "black";
+  text.style.color = "white";
+});
+paper.addEventListener("mouseout", () => {
+  const img = paper.querySelector("img");
+  const text = paper.querySelector("p");
+
+  img.src = "elements/hand-regular.svg";
+  paper.style.backgroundColor = "white";
+  text.style.color = "black";
+});
+
+scissors.addEventListener("mouseover", () => {
+  const img = scissors.querySelector("img");
+  const text = scissors.querySelector("p");
+
+  img.src = "elements/hand-scissors-solid.svg";
+  scissors.style.backgroundColor = "black";
+  text.style.color = "white";
+});
+
+scissors.addEventListener("mouseout", () => {
+  const img = scissors.querySelector("img");
+  const text = scissors.querySelector("p");
+
+  img.src = "elements/hand-scissors-regular.svg";
+  scissors.style.backgroundColor = "white";
+  text.style.color = "black";
+});
